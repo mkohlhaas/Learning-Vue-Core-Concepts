470770e2ebd833da71a32eacacc7dffaeaf34c17
Hans-J. Schmid <knock@myopendoor.de>
2025-05-30 23:38:10 +0200

    ch02 remove orig

--- ch02/orig/src/plugins/samplePlugin.ts
+++
@@ -1,7 +0,0 @@
-import type { App  } from 'vue'
-
-export default {
- install(app: App<Element>, options: Object) {
-  // Installation logic
- }
-}
--- ch02/orig/src/types/Data.type.ts
+++
@@ -1,3 +0,0 @@
-export type Data = {
-    title: string;
-}
